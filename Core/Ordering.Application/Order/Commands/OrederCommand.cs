ce5352112e235a4ea9587d4d9fa479dcfea48250
زكريا يحيى زكريا الحاج عيد <Zakariaba@moj.gov.sa>
2023-11-02 10:23:18 +0300

    Idomptency

--- Core/Ordering.Application/Order/Commands/OrederCommand.cs
+++ Core/Ordering.Application/Order/Commands/OrederCommand.cs
@@ -11,7 +11,7 @@ namespace Ordering.Application.Order.Commands
 {
     public sealed record OrederCommand(Guid requestId, string UserId, string UserName, string City, string Street, string State, string ZipCode,
         string Country, string CardNumber, string CardHolderName, DateTime CardExpiration, string CardSecurityNumber,
-        int CardTypeId, IEnumerable<OrderItemDto> orderItemDtos) : IdempotencyCommand(requestId), ICommand
+        int CardTypeId, IEnumerable<OrderItemDto> orderItemDtos) :  ICommand
     {
         public bool isTransaction => true;
 
--- OrderingSystemDDD/Configration/ApplicationServiceInstaller.cs
+++ OrderingSystemDDD/Configration/ApplicationServiceInstaller.cs
@@ -18,9 +18,11 @@ namespace OrderingSystemDDD.Configration
              //The area the use the request and request handler
               //cfg.NotificationPublisher = new ForeachAwaitPublisher();each notification handler execute one by one
              cfg.NotificationPublisher = new TaskWhenAllPublisher(); //each notification handler execute in parallel way
-             //cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
-             cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
-             cfg.AddOpenBehavior(typeof(TransactionBehavior<,>));
+                                                                     //Note :The fist pipline add is first invoke sequentially 
+                cfg.AddOpenBehavior(typeof(IdempotencyCommandBehavior<,>));
+                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
+                cfg.AddOpenBehavior(typeof(TransactionBehavior<,>));
             });
 
             services.AddValidatorsFromAssembly(AssemblyReference.assembly);
